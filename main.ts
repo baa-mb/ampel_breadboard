923a79a96212d46b041ff67cf6f1eba02d7b2cf2
baa <34665120+baa-mb@users.noreply.github.com>
2021-10-04 23:39:54 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
+function aus () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
+    aus()
     basic.showString("R")
     pins.analogWritePin(AnalogPin.P0, 512)
     basic.pause(2000)
     basic.showString("Y")
     pins.analogWritePin(AnalogPin.P1, 512)
     basic.pause(2000)
+    aus()
     basic.showString("G")
     pins.analogWritePin(AnalogPin.P2, 512)
-    basic.pause(2000)
-    pins.analogWritePin(AnalogPin.P0, 0)
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P1, 0)
     basic.pause(2000)
 })
